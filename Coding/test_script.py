9d26e894d63ae236e2ef58b94964d7387a6f63fd
Jaime <jaimescoseru@gmail.com>
2024-06-10 19:17:43 -0500

    vault backup: 2024-06-10 19:17:42
    Affected files:
    .gitignore
    .obsidian/workspace.json
    Coding/test_script.py

--- Coding/test_script.py
+++ Coding/test_script.py
@@ -1,2 +1,7 @@
+import importlib
 import pytest
+problem = importlib.import_module('217_contains_duplicate')
 
+@pytest.mark.parametrize("test,expected", problem.TEST_CASES)
+def test_resukts(test, expected):
+    print(test, expected)
