c7b601d9bfc9d3b877cf28f45287915e3c7ad175
Jaime <jaimescoseru@gmail.com>
2024-06-10 19:28:10 -0500

    vault backup: 2024-06-10 19:28:09
    Affected files:
    .obsidian/workspace.json
    Coding/test_script.py

--- Coding/test_script.py
+++ Coding/test_script.py
@@ -1,7 +1,8 @@
 import importlib
 import pytest
-problem = importlib.import_module('217_contains_duplicate')
+problem = importlib.import_module('coding.217_contains_duplicate')
 
 @pytest.mark.parametrize("test,expected", problem.TEST_CASES)
 def test_resukts(test, expected):
-    print(test, expected)
+    result = problem.main(test)
+    assert result == expected
