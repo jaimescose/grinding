3980254107555a58b19f75a23e365001af0e9a0a
Jaime <jaimescoseru@gmail.com>
2024-06-10 19:07:43 -0500

    vault backup: 2024-06-10 19:07:42
    Affected files:
    .obsidian/workspace.json
    Coding/217_contains_duplicate.py
    Coding/__init__.py
    Coding/test_script.py
    Pipfile
    Pipfile.lock

--- Coding/217_contains_duplicate.py
+++ Coding/217_contains_duplicate.py
@@ -1,5 +1,8 @@
-test_cases = [
+TEST_CASES = [
     ([1, 2, 3, 1], True),
     ([1, 2, 3, 4], False),
     ([1, 1, 1, 3, 3, 4, 3, 2, 4, 2], True),
 ]
+
+def main(numbers: list[int]) -> bool:
+    return True
