d6221010144c4a3d8c305a96d98d669161cca6eb
Jaime <jaimescoseru@gmail.com>
2024-06-20 22:20:49 -0500

    vault backup: 2024-06-20 22:20:49
    Affected files:
    .obsidian/workspace.json
    Coding/242. Valid Anagram.md
    Coding/242_valid_anagram.py
    Coding/test_script.py

--- Coding/test_script.py
+++ Coding/test_script.py
@@ -1,8 +1,9 @@
 import importlib
+
 import pytest
 problem = importlib.import_module('coding.217_contains_duplicate')
 
 @pytest.mark.parametrize("test,expected", problem.TEST_CASES)
-def test_resukts(test, expected):
+def test_results(test, expected):
     result = problem.main(test)
     assert result == expected
